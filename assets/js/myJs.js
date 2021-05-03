0b53c46e5d32fa680cd5df3313e7fa0b73c5682e
sajidur <sajidur.inbox@gmail.com>
2021-05-03 23:20:25 +0600

    init

---
+++ assets/js/myJs.js
@@ -0,0 +1,9 @@
+function showMoreWhatIDo(){
+    document.getElementById('whatIDoHidden').style.display="inline-block";
+    document.getElementById('showMoreWhatIDo-btn').style.display="none";
+}
+
+function showLessWhatIDo(){
+    document.getElementById('whatIDoHidden').style.display="none";
+    document.getElementById('showMoreWhatIDo-btn').style.display="block";
+}
